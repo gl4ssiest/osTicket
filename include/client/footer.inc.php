2618b3a10fdd5376b330d93eb0636276edf38583
Andres Rodriguez <josh.rodriguez.b@gmail.com>
2019-02-05 15:32:48 +0100

    Removing osticket logos

--- include/client/footer.inc.php
+++ include/client/footer.inc.php
@@ -2,7 +2,7 @@
     </div>
     <div id="footer">
         <p><?php echo __('Copyright &copy;'); ?> <?php echo date('Y'); ?> <?php echo (string) $ost->company ?: 'osTicket.com'; ?> - <?php echo __('All rights reserved.'); ?></p>
-        <a id="poweredBy" href="http://osticket.com" target="_blank"><?php echo __('Helpdesk software - powered by osTicket'); ?></a>
+        <!--a id="poweredBy" href="http://osticket.com" target="_blank"><?php echo __('Helpdesk software - powered by osTicket'); ?></a-->
     </div>
 <div id="overlay"></div>
 <div id="loading">
